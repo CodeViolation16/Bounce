38ad128802b0b1a2125adceb40eb0866d5b1e9cc
lucas sproule <lucas.sproule.42@gmail.com>
2024-04-08 14:49:22 -0400

    fix signup issue

--- src/pages/Homepage/HomePage.js
+++ src/pages/Homepage/HomePage.js
@@ -4,8 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { Modal } from "../components";
-import { UserContext } from "../hooks";
+import { Modal } from "../../components";
+import { UserContext } from "../../hooks";
 
 export const Homepage = () => {
   const [days, setDays] = useState();
